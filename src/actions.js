cce4c3bdc3d2a58ffd9d3b98055089b1aaaa724c
Tom Milligan <code@tommilligan.net>
2018-06-16 09:56:14 +0100

    Added actual merge

--- src/actions.js
+++ src/actions.js
@@ -2,26 +2,48 @@ const utils = require("./utils");
 
 const checkDelay = 1000;
 
+const merge_pr = async (robot, context, pr) => {
+  robot.log.info("pr is ready to go");
+  const comment = context.repo({
+    number: pr.number,
+    body: "PR is ready for merge"
+  });
+  context.github.issues.createComment(comment);
+
+  const merge = context.repo({
+    number: pr.number,
+    commit_title: `${pr.title} (#${pr.number})`,
+    sha: pr.head.sha,
+    merge_method: "squash"
+  });
+  const result = await octokit.pullRequests.merge(merge);
+};
+
 const check_pr = async (robot, context, pr) => {
   robot.log.info(`head: pr${pr.number} ${pr.head.sha}`);
 
   robot.log.info(`delaying check for ${checkDelay}ms`);
   await utils.sleep(checkDelay);
 
-  const params = context.repo({ ref: pr.head.sha });
-  const { data } = await context.github.repos.getCombinedStatusForRef(params);
-  robot.log.info(`head: pr${pr.number} ${data.state}`);
+  // check HEAD hasn't moved
+  const { pr_check } = await context.github.pullRequests.get(
+    context.repo({ number: pr.number })
+  );
+  if (pr_check.head.sha === pr.head.sha) {
+    robot.log.info(`head is still at ${pr.head.sha}`);
 
-  if (data.state === "success") {
-    robot.log.info("pr is ready to go");
-    const comment = context.repo({
-      number: pr.number,
-      body: "PR is ready for merge"
-    });
-    context.github.issues.createComment(comment);
-  } else {
-    robot.log.info("pr is not ready yet");
+    // check commit is success
+    const params = context.repo({ ref: pr.head.sha });
+    const { data } = await context.github.repos.getCombinedStatusForRef(params);
+
+    if (data.state === "success") {
+      robot.log.info(`head is ${data.state}`);
+
+      merge_pr(robot, context, pr_check);
+    }
   }
+
+  robot.log.info("pr is not ready yet");
 };
 
 const get_config = async (robot, context) => {
