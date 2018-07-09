61e4059665613ce51120ffc1c756669429bf502e
Tom Milligan <36413052+tommilligan-plutoflume@users.noreply.github.com>
2018-07-09 14:34:13 +0100

    bash var typo

--- scripts/deploy-tag.sh
+++ scripts/deploy-tag.sh
@@ -6,7 +6,7 @@ VERSION="printf \"${TRAVIS_TAG}\" | sed 's/^v//'"
 IMAGE_TAG="tommilligan/prowl-github-app:${VERSION}"
 
 # Build image
-docker build -t $IMAGE_TAG .
+docker build -t "${IMAGE_TAG}" .
 
 # Push to DockerHub
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
