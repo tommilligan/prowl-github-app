5580d549570491db65febc13737d458876fec543
Tom Milligan <36413052+tommilligan-plutoflume@users.noreply.github.com>
2018-07-09 11:18:39 +0100

    Updated deployment scripts do deploy tagged commits to docker hub

---
+++ scripts/deploy-tag.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -ev
+
+# Version tag has no 'v' prefix on docker
+VERSION="echo ${TRAVIS_TAG} | sed 's/^v//'"
+IMAGE_TAG="tommilligan/prowl-github-app:${VERSION}"
+
+# Build image
+docker build -t $IMAGE_TAG .
+
+# Push to DockerHub
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+docker push $IMAGE_TAG
+
