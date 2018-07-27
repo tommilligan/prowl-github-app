9fa12c98b66c63be9cba9deb94287eac04bbf38c
Tom Milligan <tommilligan@users.noreply.github.com>
2018-07-27 11:16:53 +0100

    Ignored all comments in github reviews, which override actual important states

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,14 @@
 const APP_NAME = 'prowl'
+const GITHUB_PR_REVIEWS_STATES = {
+  PASS: ['APPROVED', 'DISMISSED'],
+  // use !PASS instead
+  // FAIL: ['CHANGES_REQUESTED'],
+  IGNORE: ['COMMENTED']
+}
 const LINK_README_COMMANDS = 'https://github.com/tommilligan/prowl-github-app#use'
 
 module.exports = {
   APP_NAME,
+  GITHUB_PR_REVIEWS_STATES,
   LINK_README_COMMANDS
 }
